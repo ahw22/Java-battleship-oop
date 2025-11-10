07b1fbae2efa6d7c97920708a2842ab34da16edb
ahwz <andreas.zincke@gmx.at>
2025-11-10 10:18:23 +0100

    add missing imports

--- src/main/java/org/example/commands/Command.java
+++ src/main/java/org/example/commands/Command.java
@@ -1,5 +1,7 @@
 package org.example.commands;
 
+import org.example.game.GameContext;
+
 public interface Command {
 
     void execute(GameContext context);
--- src/main/java/org/example/input/InputHandler.java
+++ src/main/java/org/example/input/InputHandler.java
@@ -1,5 +1,7 @@
 package org.example.input;
 
+import org.example.commands.Command;
+
 public interface InputHandler {
 
     Command getNextCommand();
