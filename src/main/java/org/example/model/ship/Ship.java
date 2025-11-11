2054a5db5b8d341dfd7ecf29a4a17eb8bdfd9d85
ahwz <andreas.zincke@gmx.at>
2025-11-11 14:58:29 +0100

    refactor to use lombok

--- src/main/java/org/example/model/ship/Ship.java
+++ src/main/java/org/example/model/ship/Ship.java
@@ -1,10 +1,12 @@
 package org.example.model.ship;
 
+import lombok.Getter;
 import org.example.model.board.Field;
 
 import java.util.ArrayList;
 
 public class Ship {
+     @Getter
      protected int HP;
      protected ArrayList<Field> fields;
 
