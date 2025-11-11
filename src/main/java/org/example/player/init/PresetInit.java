a5edf0256540293594582a40f085f3ac60ee2f47
ahwz <andreas.zincke@gmx.at>
2025-11-11 15:08:01 +0100

    start place PresetInit implementation //TODO

---
+++ src/main/java/org/example/player/init/PresetInit.java
@@ -0,0 +1,11 @@
+package org.example.player.init;
+
+import org.example.model.board.Board;
+import org.example.player.Player;
+
+public class PresetInit implements BoardInitializer{
+    @Override
+    public Board initBoard(Player player) {
+        return null;
+    }
+}
