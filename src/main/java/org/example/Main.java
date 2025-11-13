914136a4358a079389814d90033e4e2ad5c612a3
ahwz <andreas.zincke@gmx.at>
2025-11-13 15:05:21 +0100

    clean out main for now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,6 @@
 package org.example;
 
-import org.example.input.UserInputHandler;
-import org.example.player.Player;
-import org.example.player.init.ManualInit;
-
 public class Main {
     public static void main(String[] args) {
-        UserInputHandler inputHandler = new UserInputHandler(System.in);
-        ManualInit manualInit = new ManualInit(inputHandler);
-
-        Player player1 = new Player(manualInit); // inject the board initializer
-
-        player1.getBoard().printBoard(); // assuming your Board has a printBoard method
     }
 }
