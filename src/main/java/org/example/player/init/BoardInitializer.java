ab59c7c8f6c35f0eb266ccfe6ba101bfc9946adf
ahwz <andreas.zincke@gmx.at>
2025-11-13 15:03:56 +0100

    tidy up BoardInitializer and start PresetInit implementation

--- src/main/java/org/example/player/init/BoardInitializer.java
+++ src/main/java/org/example/player/init/BoardInitializer.java
@@ -1,11 +1,8 @@
 package org.example.player.init;
 
 import org.example.model.board.Board;
-import org.example.model.ship.Ship;
 import org.example.player.Player;
 
 public interface BoardInitializer {
     Board initBoard(Player player);
-    void placeShip(Ship ship, Board board);
-
 }
--- src/main/java/org/example/player/init/PresetInit.java
+++ src/main/java/org/example/player/init/PresetInit.java
@@ -1,11 +1,16 @@
 package org.example.player.init;
 
 import org.example.model.board.Board;
+import org.example.model.ship.Ship;
 import org.example.player.Player;
+
+import java.util.List;
 
 public class PresetInit implements BoardInitializer{
     @Override
     public Board initBoard(Player player) {
+        List<Ship> ships = player.getShips();
+
         return null;
     }
 }
