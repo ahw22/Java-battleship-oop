1407e6b3ce62909d2c5953983ba76f27ee300076
ahwz <andreas.zincke@gmx.at>
2025-11-13 15:05:05 +0100

    add shipsToPlace queue and fill it on creation

--- src/main/java/org/example/player/Player.java
+++ src/main/java/org/example/player/Player.java
@@ -6,17 +6,25 @@ import org.example.player.init.BoardInitializer;
 import org.example.model.board.Board;
 import org.example.model.ship.*;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 @Getter
 @Builder
 public class Player {
     private final String name;
     private final Board ownBoard;
     private final Board targetBoard;
+    private final List<Ship> ships;
+    private final Queue<Ship> shipsToPlace = new LinkedList<>();
 
     public Player(BoardInitializer initializer, String name) {
         this.ownBoard = initializer.initBoard(this);
         this.targetBoard = new Board();
         this.name = name;
+        this.ships = List.of(new Battleship(), new Carrier(), new Destroyer(), new Submarine());
+        shipsToPlace.addAll(ships);
     }
 
 }
