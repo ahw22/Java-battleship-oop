a0854209e2537971093b55e641035ec6c7e83303
ahwz <andreas.zincke@gmx.at>
2025-11-17 15:13:35 +0100

    fix bug in Player that made it so the board would init empty cause the ships didn't exist yet

--- src/main/java/org/example/player/Player.java
+++ src/main/java/org/example/player/Player.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Queue;
 
 @Getter
-@Builder
 public class Player {
     private final String name;
     private final Board ownBoard;
@@ -20,11 +19,11 @@ public class Player {
     private final Queue<Ship> shipsToPlace = new LinkedList<>();
 
     public Player(BoardInitializer initializer, String name) {
-        this.ownBoard = initializer.initBoard(this);
         this.targetBoard = new Board();
         this.name = name;
         this.ships = List.of(new Battleship(), new Carrier(), new Destroyer(), new Submarine());
         shipsToPlace.addAll(ships);
+        this.ownBoard = initializer.initBoard(this);
     }
 
 }
