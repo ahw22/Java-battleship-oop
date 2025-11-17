4bfa4c64f77add9d6328a2809365031516c854bc
ahwz <andreas.zincke@gmx.at>
2025-11-17 15:14:06 +0100

    implement main for testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,14 @@
 package org.example;
 
+import org.example.game.GameContext;
+import org.example.player.Player;
+import org.example.player.init.PresetInit;
+
 public class Main {
     public static void main(String[] args) {
+        Player player1 = new Player(new PresetInit(), "User");
+        Player player2 = new Player(new PresetInit(), "CPU");
+
+        GameContext context = new GameContext(player1, player2);
     }
 }
