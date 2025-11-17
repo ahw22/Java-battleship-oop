f66a3f34a1aa923f6dd3819c0233f7dc246a9ffb
ahwz <andreas.zincke@gmx.at>
2025-11-17 14:05:26 +0100

    implement PresetInit

--- src/main/java/org/example/player/init/PresetInit.java
+++ src/main/java/org/example/player/init/PresetInit.java
@@ -1,16 +1,28 @@
 package org.example.player.init;
 
 import org.example.model.board.Board;
+import org.example.model.board.Position;
 import org.example.model.ship.Ship;
 import org.example.player.Player;
 
-import java.util.List;
 
 public class PresetInit implements BoardInitializer{
+    private Board board;
+
+    public PresetInit(Board board) {
+        this.board = board;
+    }
+
     @Override
     public Board initBoard(Player player) {
-        List<Ship> ships = player.getShips();
-
-        return null;
+        while (!player.getShipsToPlace().isEmpty()) {
+        Ship peek = player.getShipsToPlace().peek();
+        char targetRow = (char) ('A' + peek.getHP());
+        String start = String.valueOf(targetRow + 1);
+        String end = String.valueOf(targetRow + peek.getHP());
+        board.placeShip(convertStringToPosition(start), convertStringToPosition(end), player);
+        }
+        board.printBoard();
+        return board;
     }
 }
