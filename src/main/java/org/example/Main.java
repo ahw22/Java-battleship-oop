0e641c7c29f9f7a23a70e4c930bec683aa3311e7
ahwz <andreas.zincke@gmx.at>
2025-11-19 15:02:52 +0100

    more manual testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.game.GameContext;
+import org.example.model.board.Position;
 import org.example.player.Player;
 import org.example.player.init.PresetInit;
 
@@ -8,7 +9,13 @@ public class Main {
     public static void main(String[] args) {
         Player player1 = new Player(new PresetInit(), "User");
         Player player2 = new Player(new PresetInit(), "CPU");
+        player1.setTargetBoard(player2.getOwnBoard());
+        player2.setTargetBoard(player1.getOwnBoard());
 
         GameContext context = new GameContext(player1, player2);
+        context.showBoards();
+        context.fireAt(new Position(2,2));
+        context.fireAt(new Position(2,1));
+        context.showBoards();
     }
 }
