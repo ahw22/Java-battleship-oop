03b3b78bd52d7bea7840a4f6837269cde1f707f1
ahwz <andreas.zincke@gmx.at>
2025-11-20 11:24:18 +0100

    rename column attribute for readability and change ToString to be preferred output.

--- src/main/java/org/example/model/board/Position.java
+++ src/main/java/org/example/model/board/Position.java
@@ -1,36 +1,27 @@
 package org.example.model.board;
 
+import lombok.Getter;
+
 import java.util.Random;
 
+@Getter
 public class Position {
     private final int row;
-    private final int col;
+    private final int column;
 
     public Position() {
         Random rand = new Random();
         this.row = rand.nextInt(0, 10);
-        this.col = rand.nextInt(0, 10);
+        this.column = rand.nextInt(0, 10);
     }
 
     public Position(int col, int row) {
         this.row = row;
-        this.col = col;
+        this.column = col;
     }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return col;
-    }
-
 
     @Override
     public String toString() {
-        return "Position{" +
-                "col=" + col +
-                ", row=" + row +
-                '}';
+        return "" + (char)('A' + column) + row;
     }
 }
