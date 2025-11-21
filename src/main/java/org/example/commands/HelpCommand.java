8f5e13875858db7ab7c2099e573799ee525ce3f9
ahwz <andreas.zincke@gmx.at>
2025-11-21 11:01:42 +0100

    change help command

--- src/main/java/org/example/commands/HelpCommand.java
+++ src/main/java/org/example/commands/HelpCommand.java
@@ -9,7 +9,7 @@ public class HelpCommand implements Command{
                 The following commands are available:
                 help: used to show this list of commands.
                 fire: used to fire at enemy board. Example: fire B3
-                show: used to show both your and the enemy board.
+                show: used to show your board.
                 place: used when placing ships before the game. Choose start and endpoint of your ship. Example: place A3 C6
                 quit: Exits the game.
                 """);;
