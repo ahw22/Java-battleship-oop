25e75daaba1f2f66a78fead69deea96307de2f91
ahwz <andreas.zincke@gmx.at>
2025-12-03 12:44:00 +0100

    add test of full game with 2 random players

---
+++ src/test/java/org/example/RandomTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.example.game.GameContext;
+import org.example.game.GameRunner;
+import org.example.input.RandomInputHandler;
+import org.example.input.TestInputHandler;
+import org.example.player.Player;
+import org.example.player.init.PresetInit;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RandomTest {
+
+    @Test
+    void testFullGame() {
+        Player p1 = new Player(new PresetInit(), new RandomInputHandler(), "CPU1");
+        Player p2 = new Player(new PresetInit(), new RandomInputHandler(), "CPU2");
+        p1.setTargetBoard(p2.getOwnBoard());
+        p2.setTargetBoard(p1.getOwnBoard());
+
+        GameContext ctx = new GameContext(p1, p2, System.out);
+
+        GameRunner runner = new GameRunner(System.out);
+        runner.run(ctx);
+
+    }
+
+}
